46c76e1098bd9bf161d6cf8f988132885baee36b
Renuka Prasad A <arenukaprasad@gmail.com>
2017-08-01 20:13:02 +0530

    added solution to reverse string problem

---
+++ reverse_a_string.js
@@ -0,0 +1,27 @@
+// Reverse the provided string.
+
+
+
+// Your result must be a string.
+
+
+/*
+Pseudocode:
+1. separate the string with string.split() method
+2. reverse the returned array by split method using array.reverse() method
+3. combine the array elements to form reversed string using array.join() method
+*/
+
+
+function reverseString(str){
+
+    var reverseString= str.split('').reverse().join('');
+
+     console.log(reverseString);
+
+    return reverseString;
+
+   
+}
+
+reverseString('redemption')
